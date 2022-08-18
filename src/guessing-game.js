6834fe2c9d402ef9aa0020e3b5c8423e7de9cc15
Anneli Savchenko <95057484+Anneli-sf@users.noreply.github.com>
2022-08-18 05:22:13 -0700

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    return this.middle = Math.floor((this.min + this.max) / 2);
+  }
 
-    guess() {
+  lower() {
+    return this.max = this.middle - 1;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    return this.min = this.middle + 1;
+  }
 }
 
 module.exports = GuessingGame;
